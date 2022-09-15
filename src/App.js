c28cbfe86cf9b82a7a02dd44ea9b996ebeda1c50
Alper <as.alper1@hotmail.com>
2022-09-15 09:14:19 +0300

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import { About } from "./About";
 import { Background } from "./Background";
-
+import { Git } from "./Git";
 
 function App() {
   return (
-    <div className="h-screen w-[100%]">
-      <Background/>
+    <div className="h-full w-[100%] pb-10">
+      <Background />
+      <div className="h-full grid lg:grid-cols-2 sm:grid-cols-1">
+        <About />
+        <Git />
+      </div>
     </div>
   );
 }
