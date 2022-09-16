132a3ff904d5323c9e3c524bcb66e0c9cd36e2ba
Alper <as.alper1@hotmail.com>
2022-09-16 08:56:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import { About } from "./About";
+import { Projects } from "./Projects";
 import { Background } from "./Background";
 import { Git } from "./Git";
+import { Acapnes } from "./Acapnes";
 
 function App() {
   return (
     <div className="h-full w-[100%] pb-10">
       <Background />
-      <div className="h-full grid lg:grid-cols-2 sm:grid-cols-1">
-        <About />
+      <div className="h-full grid grid-rows">
+        <Acapnes/>
+        <Projects />
         <Git />
       </div>
     </div>
