a47cd7fc7f7aff33d3cec19c9957965d72ced23d
Alper <as.alper1@hotmail.com>
2022-09-17 19:35:44 +0300

    Create Git.js

---
+++ src/Git.js
@@ -0,0 +1,190 @@
+import React from "react";
+
+export const Git = () => {
+  return (
+    <div className="p-5 grid grid-rows">
+      <div className="p-5 text-white text-4xl font-bold items-center flex justify-center border-b-2 border-white mb-5">GitHub Public Repositories</div>
+
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+      <div className="p-8 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4 items-center text-white hover:bg-gray-900 hover:bg-opacity-50">
+        <div className="flex flex-col">
+          <p className="text-3xl font-semibold text-orange-400">
+            Lost or Found
+          </p>
+          <p className="text-lg font-light">
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer Junior Full-Stack Developer Junior Full-Stack
+            Developer Junior Full-Stack Developer Junior Full-Stack Developer
+            Junior Full-Stack Developer Junior Full-Stack Developer Junior
+            Full-Stack Developer{" "}
+          </p>
+        </div>
+        <div className="flex justify-center"></div>
+      </div>
+    </div>
+  );
+};
